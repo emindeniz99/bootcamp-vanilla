5d3a9e1da9277ec44ae70332aa5c18e4f31ca1bb
Emin Deniz <emindeniz99@gmail.com>
2021-09-11 12:07:59 +0300

    feat: create currency table
    add currency data and show them at a table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import data from "./data";
+
 const App = () => {
   return (
     <div className="App">
-      App
+      <CurrencyTable currencies={data.currencies} />
     </div>
   );
-}
+};
 
+const CurrencyTable = ({ currencies }) => {
+  const fields = {
+    exchangeType: "Döviz Cinsi",
+    buy: "Alış(TL)",
+    sell: "Satış(TL)",
+    diff: "Fark(%)",
+  };
+  return (
+    <>
+      <Table>
+        <thead>
+          <tr>
+            {Object.values(fields).map((val, index) => (
+              <HeadCell key={index}>{val}</HeadCell>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {currencies.map((currency, index) => (
+            <tr key={index}>
+              {Object.keys(fields).map((field, index) => (
+                <DataCell key={index}>{currency[field]}</DataCell>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+const Table = ({ children }) => {
+  return <table style={{ border: "1px solid black" }}>{children}</table>;
+};
+const DataCell = ({ children }) => {
+  return <td style={{ border: "1px solid black" }}>{children}</td>;
+};
+const HeadCell = ({ children }) => {
+  return <th style={{ border: "1px solid black" }}>{children}</th>;
+};
 export default App;
